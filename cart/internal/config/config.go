6bf6b2b52cb1e5c77ea646b102a5cfbc444d5f90
Arutyunyan A <a.arutyunyan@tanais.ru>
2023-09-21 15:39:18 +0300

    change srv ports

--- cart/internal/config/config.go
+++ cart/internal/config/config.go
@@ -39,7 +39,7 @@ func New() (*Config, error) {
 
 func (c *Config) SetDefaultValues() {
 	c.Server.Addr = ":8080"
-	c.LOMS.Addr = "http://loms:8083"
+	c.LOMS.Addr = "http://loms:8080"
 	c.PIM.Addr = "http://route256.pavl.uk:8080"
 }
 
