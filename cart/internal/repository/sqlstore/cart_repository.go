5dfde1c6b87ae220086e364f8372ee471d628f53
Arutyunyan A <casnerano@mail.ru>
2023-10-01 23:55:34 +0300

    impl sql repo

--- cart/internal/repository/sqlstore/cart_repository.go
+++ cart/internal/repository/sqlstore/cart_repository.go
@@ -19,18 +19,55 @@ func NewCartRepository(pgxpool *pgxpool.Pool) repository.Cart {
 
 var _ repository.Cart = (*cartRepository)(nil)
 
-func (rep *cartRepository) Add(_ context.Context, userID model.UserID, sku model.SKU, count uint32) (*model.Item, error) {
-	return nil, nil
+func (rep *cartRepository) Add(ctx context.Context, userID model.UserID, sku model.SKU, count uint32) (*model.Item, error) {
+	row := rep.pgxpool.QueryRow(ctx, "INSERT INTO cart (user_id, sku_id, count) VALUES ($1, $2, $3) RETURNING id, user_id, sku_id, count", userID, sku, count)
+	var item model.Item
+	err := row.Scan(
+		&item.ID,
+		&item.UserID,
+		&item.SKU,
+		&item.Count,
+	)
+	return &item, err
 }
 
 func (rep *cartRepository) FindByUser(ctx context.Context, userID model.UserID) ([]*model.Item, error) {
-	return nil, repository.ErrNotFound
+	rows, err := rep.pgxpool.Query(ctx, "SELECT id, user_id, sku_id, count FROM cart where user_id = $1", userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var items []*model.Item
+
+	for rows.Next() {
+		var item model.Item
+		err = rows.Scan(
+			&item.ID,
+			&item.UserID,
+			&item.SKU,
+			&item.Count,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		items = append(items, &item)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
 
 func (rep *cartRepository) DeleteBySKU(ctx context.Context, userID model.UserID, sku model.SKU) error {
-	return repository.ErrNotFound
+	_, err := rep.pgxpool.Exec(ctx, "DELETE FROM cart WHERE user_id = $1 and sku_id = $2", userID, sku)
+	return err
 }
 
 func (rep *cartRepository) DeleteByUser(ctx context.Context, userID model.UserID) error {
-	return repository.ErrNotFound
+	_, err := rep.pgxpool.Exec(ctx, "DELETE FROM cart WHERE user_id = $1", userID)
+	return err
 }
