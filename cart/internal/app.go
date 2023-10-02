3c779f5eb5a68639b7299e6a1ef07d3ab6eed4e5
Arutyunyan A <casnerano@mail.ru>
2023-10-02 23:55:09 +0300

    added migrations for cart and init repo

--- cart/internal/app.go
+++ cart/internal/app.go
@@ -1,110 +1,111 @@
 package internal
 
 import (
-	"context"
-	"fmt"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"route256/cart/internal/config"
-	"route256/cart/internal/repository"
-	"route256/cart/internal/repository/memstore"
-	"route256/cart/internal/repository/sqlstore"
-	"route256/cart/internal/server"
-	"route256/cart/internal/service/cart"
-	"route256/cart/internal/service/client/loms"
-	"route256/cart/internal/service/client/pim"
+    "context"
+    "fmt"
+    "github.com/jackc/pgx/v5/pgxpool"
+    "route256/cart/internal/config"
+    "route256/cart/internal/repository"
+    "route256/cart/internal/repository/memstore"
+    "route256/cart/internal/repository/sqlstore"
+    "route256/cart/internal/server"
+    "route256/cart/internal/service/cart"
+    "route256/cart/internal/service/client/loms"
+    "route256/cart/internal/service/client/pim"
 )
 
 type depRepository struct {
-	cart repository.Cart
+    cart repository.Cart
 }
 
 type depService struct {
-	cart       *cart.Cart
-	pimClient  *pim.Client
-	lomsClient *loms.Client
+    cart       *cart.Cart
+    pimClient  *pim.Client
+    lomsClient *loms.Client
 }
 
 type application struct {
-	config        *config.Config
-	server        *server.Server
-	depRepository *depRepository
-	depService    *depService
+    config        *config.Config
+    server        *server.Server
+    depRepository *depRepository
+    depService    *depService
 }
 
 func NewApp() (*application, error) {
-	var err error
-	app := application{}
+    var err error
+    app := application{}
 
-	app.config, err = config.New()
-	if err != nil {
-		return nil, err
-	}
+    app.config, err = config.New()
+    if err != nil {
+        return nil, err
+    }
 
-	var cartRepo repository.Cart
-	if app.config.Database.DSN != "" {
-		var pool *pgxpool.Pool
-		pool, err = pgxpool.New(context.TODO(), app.config.Database.DSN)
-		if err != nil {
-			return nil, err
-		}
-		cartRepo = sqlstore.NewCartRepository(pool)
-	} else {
-		cartRepo = memstore.NewCartRepository()
-	}
+    var cartRepo repository.Cart
 
-	app.depRepository = &depRepository{
-		cart: cartRepo,
-	}
+    if app.config.Database.DSN != "" {
+        var pool *pgxpool.Pool
+        pool, err = pgxpool.New(context.TODO(), app.config.Database.DSN)
+        if err != nil {
+            return nil, err
+        }
+        cartRepo = sqlstore.NewCartRepository(pool)
+    } else {
+        cartRepo = memstore.NewCartRepository()
+    }
 
-	pimClient := pim.NewClient(app.config.PIM.Addr)
+    app.depRepository = &depRepository{
+        cart: cartRepo,
+    }
 
-	lomsClient, err := loms.NewClient(app.config.LOMS.Addr)
-	if err != nil {
-		return nil, err
-	}
+    pimClient := pim.NewClient(app.config.PIM.Addr)
 
-	app.depService = &depService{
-		pimClient:  pimClient,
-		lomsClient: lomsClient,
-	}
+    lomsClient, err := loms.NewClient(app.config.LOMS.Addr)
+    if err != nil {
+        return nil, err
+    }
 
-	app.depService.cart = cart.New(
-		app.depRepository.cart,
-		app.depService.pimClient,
-		app.depService.lomsClient,
-	)
+    app.depService = &depService{
+        pimClient:  pimClient,
+        lomsClient: lomsClient,
+    }
 
-	err = app.init()
-	if err != nil {
-		return nil, fmt.Errorf("failed init server: %w", err)
-	}
+    app.depService.cart = cart.New(
+        app.depRepository.cart,
+        app.depService.pimClient,
+        app.depService.lomsClient,
+    )
 
-	return &app, nil
+    err = app.init()
+    if err != nil {
+        return nil, fmt.Errorf("failed init server: %w", err)
+    }
+
+    return &app, nil
 }
 
 func (a *application) init() error {
-	var err error
-	a.server, err = server.New(a.config.Server, a.depService.cart)
+    var err error
+    a.server, err = server.New(a.config.Server, a.depService.cart)
 
-	return err
+    return err
 }
 
 func (a *application) RunGRPCServer() error {
-	return a.server.RunGRPC()
+    return a.server.RunGRPC()
 }
 
 func (a *application) RunHTTPServer() error {
-	return a.server.RunHTTP()
+    return a.server.RunHTTP()
 }
 
 func (a *application) Shutdown() error {
-	if err := a.depService.lomsClient.Close(); err != nil {
-		return nil
-	}
+    if err := a.depService.lomsClient.Close(); err != nil {
+        return nil
+    }
 
-	if err := a.server.ShutdownHTTP(); err != nil {
-		return err
-	}
+    if err := a.server.ShutdownHTTP(); err != nil {
+        return err
+    }
 
-	return a.server.ShutdownGRPC()
+    return a.server.ShutdownGRPC()
 }
