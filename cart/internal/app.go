039e28f8c7269247b54982cf49eea519036d6d3c
Arutyunyan A <casnerano@mail.ru>
2023-11-05 16:21:55 +0300

    bugfix: pgx config for pgbouncer

--- cart/internal/app.go
+++ cart/internal/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/exaring/otelpgx"
+	"github.com/jackc/pgx/v5"
 	"route256/cart/internal/config"
 	"route256/cart/internal/repository"
 	"route256/cart/internal/repository/sqlstore"
@@ -72,6 +73,7 @@ func NewApp() (*application, error) {
 	}
 
 	pgxConfig.ConnConfig.Tracer = otelpgx.NewTracer()
+	pgxConfig.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeExec
 
 	pool, err = pgxpool.NewWithConfig(context.Background(), pgxConfig)
 	if err != nil {
--- loms/internal/app.go
+++ loms/internal/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/exaring/otelpgx"
+	"github.com/jackc/pgx/v5"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.uber.org/zap"
 	"route256/cart/pkg/logger"
@@ -79,6 +80,7 @@ func NewApp() (*application, error) {
 	}
 
 	pgxConfig.ConnConfig.Tracer = otelpgx.NewTracer()
+	pgxConfig.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeExec
 
 	pool, err = pgxpool.NewWithConfig(context.Background(), pgxConfig)
 	if err != nil {
